869f3d5b6928e4f9df84dc061fef6bac0b6034c4
elyaminedev <elyamine.dev@outlook.com>
2022-01-13 16:05:16 +0100

    123

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 const mysql = require('mysql');
+// query that inserts user registration data inside of a new row
 const newuserquery = "insert into sql4464870.Student (firstName, lastName, email, hashvalue)"
                     +"values (?, ?, ?, ?);";
+
+// testing query
 const testquery = "insert into sql4464870.Student (firstName, lastName)"
                     +"values ('yahya', 'elyamine');";
+const loginquery = "select id from sql4464870.Student where email = ? and hashvalue = ?;"
 
+// db connection
 const pool = mysql.createPool({
     connectionLimit: 10,
     user: 'sql4464870',
@@ -13,20 +18,35 @@ const pool = mysql.createPool({
     port: '3306'
 });
 
+// new obj
 let students = {};
 
 // create new account
 students.newUser = (fname, lname, mail, pass) =>{
     
+    // promise allowing asynchrony
     return new Promise((resolve, reject) => {
         pool.query(newuserquery, [fname, lname, mail, pass], (err, results) => {
             if(err)
-                return reject (err)
+                return reject (err);
         
             return resolve(results);
         });
     });
 };
-// login
 
+students.login = (email, pass) =>{
+    
+    // promise allowing asynchrony
+    return new Promise((resolve, reject) => {
+        pool.query(loginquery, [email, pass], (err, results) => {
+            if(err)
+                return reject (err);
+        
+            return resolve(results);
+        });
+    });
+};
+
+// exporting students obj
 module.exports = students;
--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 
-var student = require('./db');
-var bodyParser = require('body-parser');
-const express = require("express");
-const cors = require("cors");
-const app = express();
+var student = require('./db'); // db object
+var bodyParser = require('body-parser'); // handling json
+const express = require("express"); // server
+const cors = require("cors"); // infrastructure allowing request from/to different domains
+const app = express(); // instance of the server
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;  // server port
 
 
 
 
 
-
+// testing
 app.get('/test', function(req, res) {
     res.json({ 
         "x":{}
   });
 });
 
+//api post, retrieves data from the call and creates a new account inside of the database
 app.post('/account', function(req, res) {
     var fname = req.body.fname;
     var lname = req.body.lname;
@@ -29,14 +30,38 @@ app.post('/account', function(req, res) {
     var pass = req.body.pass;
     
 
-    student.newUser(fname, lname, mail, pass);
+    student.newUser(fname, lname, mail, pass); // calls the method from db obj
+    
     res.json({ 
-        "x":{}
+        "x": '2'
   });
     
   });
 
+  app.post('/login', function(req, res) {
+    var email = req.body.email;
+    var pass = req.body.pass;
+    
 
+    student.login(email, pass).then(
+        value => {
+            if(value == {})
+            res.json({
+                "x" : '0' // returns 0 if password/email are mistaken
+            });
+            else
+            res.json({
+                "x" : value.id // return id > 0 if there are the inserted credentials
+            });
+        }); 
+            
+        
+    
+    // calls the method from db obj
+    
+  });
+
+// server is listening
 app.listen(PORT, function() {
     console.log('Server listening on ' + PORT);
 });
